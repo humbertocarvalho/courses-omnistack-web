e330eef187729cedc4f86f1ad8a25b0069961304
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-08 22:05:34 -0300

    Add login component.

---
+++ src/pages/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import './Login.css';
+// import { Container } from './styles';
+
+export default function Login() {
+  return <img src={logo} alt='Tindev' />;
+}

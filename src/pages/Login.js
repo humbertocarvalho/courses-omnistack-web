83977039a6140119ad73874844b12ec34f53d702
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-08-09 19:08:46 -0300

    Add style.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo.svg';
 import './Login.css';
+import api from '../services/api';
 // import { Container } from './styles';
 
-export default function Login() {
+export default function Login({ history }) {
+  const [username, setUsername] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await api.post('/devs', {
+      username
+    });
+    console.log(response);
+
+    const { _id } = response.data;
+
+    history.push(`dev/${_id}`);
+  }
+
   return (
     <div className='login-container'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt='Tindev' />
-        <input placeholder='Digite seu usuário do Github' />
+        <input
+          placeholder='Digite seu usuário do Github'
+          value={username}
+          onChange={e => {
+            setUsername(e.target.value);
+          }}
+        />
         <button type='submit'>Enviar</button>
       </form>
     </div>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,93 @@
 import React from 'react';
+import logo from '../assets/logo.svg';
+import dislike from '../assets/dislike.svg';
+import like from '../assets/like.svg';
+import './Main.css';
 
 // import { Container } from './styles';
 
-export default function Main() {
-  return <h1>Hello WOrld</h1>;
+export default function Main({ match }) {
+  return (
+    <div className='main-container'>
+      <img src={logo} alt='Tindev' />
+      <ul>
+        <li>
+          <img
+            src='https://avatars0.githubusercontent.com/u/4248081?v=4'
+            alt=''
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Um cara legal</p>
+          </footer>
+
+          <div className='buttons'>
+            <button type='button'>
+              <img src={dislike} alt='' />
+            </button>
+            <button type='button'>
+              <img src={like} alt='' />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src='https://avatars0.githubusercontent.com/u/4248081?v=4'
+            alt=''
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Um cara legal</p>
+          </footer>
+
+          <div className='buttons'>
+            <button type='button'>
+              <img src={dislike} alt='' />
+            </button>
+            <button type='button'>
+              <img src={like} alt='' />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src='https://avatars0.githubusercontent.com/u/4248081?v=4'
+            alt=''
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Um cara legal</p>
+          </footer>
+
+          <div className='buttons'>
+            <button type='button'>
+              <img src={dislike} alt='' />
+            </button>
+            <button type='button'>
+              <img src={like} alt='' />
+            </button>
+          </div>
+        </li>
+        <li>
+          <img
+            src='https://avatars0.githubusercontent.com/u/4248081?v=4'
+            alt=''
+          />
+          <footer>
+            <strong>Filipe Deschamps</strong>
+            <p>Um cara legal</p>
+          </footer>
+
+          <div className='buttons'>
+            <button type='button'>
+              <img src={dislike} alt='' />
+            </button>
+            <button type='button'>
+              <img src={like} alt='' />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
